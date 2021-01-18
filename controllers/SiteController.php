1fa039a27b3960c4f98721b925dcf22140d06b7c
BuildTools <unconfigured@null.spigotmc.org>
2021-01-18 18:47:22 +0100

    contact form update

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -5,6 +5,8 @@ namespace app\controllers;
 use app\core\Application;
 use app\core\Controller;
 use app\core\Request;
+use app\core\Response;
+use app\models\ContactForm;
 
 class SiteController extends Controller
 {
@@ -17,11 +19,18 @@ class SiteController extends Controller
         return $this->render("home", $params);
     }
 
-    public function contact(Request $request)
+    public function contact(Request $request, Response $response)
     {
+        $contact = new ContactForm();
         if ($request->getMethod() === "post") {
-            return "handling request...";
+            $contact->loadData($request->getBody());
+            if ($contact->validate() && $contact->send()) {
+                Application::$app->session->setFlash("success", "Thanks for contacting us.");
+                $response->redirect("/");
+            }
         }
-        return $this->render("contact");
+        return $this->render("contact", [
+            "model" => $contact
+        ]);
     }
 }
