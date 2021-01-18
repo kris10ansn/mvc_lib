231b838caf1978a133dd1006f5b1d2e6b8d52dfb
BuildTools <unconfigured@null.spigotmc.org>
2021-01-18 18:47:13 +0100

    contact form update

---
+++ core/form/InputField.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace app\core\form;
+
+
+use app\core\Model;
+
+class Field
+{
+    public const TYPE_TEXT = "text";
+    public const TYPE_PASSWORD = "password";
+
+    private Model $model;
+    public string $attribute;
+    public string $type;
+
+    /**
+     * Field constructor.
+     * @param Model $model
+     * @param string $attribute
+     */
+    public function __construct(Model $model, string $attribute)
+    {
+        $this->model = $model;
+        $this->attribute = $attribute;
+        $this->type = self::TYPE_TEXT;
+    }
+
+    public function password(): Field
+    {
+        $this->type = self::TYPE_PASSWORD;
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return sprintf('
+            <label>
+                %s
+                <input type="%s" name="%s" value="%s" class="%s" autocomplete="off">
+                <p class="error">%s</p>
+            </label>
+        ',
+            $this->model->getLabel($this->attribute),
+            $this->type,
+            $this->attribute,
+            $this->model->{$this->attribute},
+            $this->model->hasError($this->attribute) ? "invalid" : "",
+            $this->model->hasError($this->attribute) ? "*{$this->model->getFirstError($this->attribute)}" : ""
+        );
+    }
+}
--- views/contact.php
+++ views/contact.php
@@ -1,14 +1,23 @@
 <?php
 /** @var $this View */
+/** @var Model $model */
 
+use app\core\form\Form;
+use app\core\Model;
 use app\core\View;
 
 $this->title = "Contact";
+
+$form = new Form("", "post", $model);
 ?>
 
 <h1>Contact</h1>
 
-<form method="POST">
-    <input type="text" placeholder="Text input" name="text">
-    <button type="submit">Submit form</button>
-</form>
+<?php
+$form->begin();
+?>
+<?php echo $form->field("subject"); ?>
+<?php echo $form->field("email") ?>
+<?php echo $form->field("body") ?>
+    <button type="submit">Submit</button>
+<?php $form->end(); ?>
